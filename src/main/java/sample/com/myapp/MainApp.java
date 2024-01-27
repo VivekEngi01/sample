57f78ecbc56c9ae6f4e11b0fce8b19ec1c6dace1
vivek <vivek@192.168.2.41>
2024-01-27 14:27:14 -0500

    First test commit

---
+++ src/main/java/sample/com/myapp/MainApp.java
@@ -0,0 +1,10 @@
+package sample.com.myapp;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Vivek G!");
+	}
+
+}
